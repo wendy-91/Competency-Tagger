2e3202a7edbb21b2d0fc9f45187c0513c5c9e582
wendy-91 <wendyyap.01@gmail.com>
2024-11-04 19:14:53 +0800

    Update Methodology.py

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -1,1 +1,8 @@
+import streamlit as st
+
+# Title of the app
+st.title("Competency Tagger App")
+
+# Display an image
+st.image(""C:\Users\wendy\Downloads\Competency Tagger Flowchart.jpg"", use_column_width=True)
 
