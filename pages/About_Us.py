d09993060dafadd6cecd16901968048639371a8b
tipsyelifant <deirepromissio@gmail.com>
2024-11-06 12:03:08 +0800

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -1,1 +1,2 @@
-
+st.title("Project Scope:")
+st.write("The Competency Tagger project aims to develop an AI-powered platform to automate the process of tagging learning programs with relevant competencies from various Competency Frameworks. This will streamline program management for Institute Development Officers (IDOs), freeing up their time for higher-value tasks like program development and evaluation.")
