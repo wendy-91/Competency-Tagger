b8a6a8451124a8aceeda0f82f42dd7df8c31c9aa
tipsyelifant <deirepromissio@gmail.com>
2024-11-06 11:49:54 +0800

    Update Competency_Tagger_App.py update disclaimer

--- Competency_Tagger_App.py
+++ Competency_Tagger_App.py
@@ -15,6 +15,7 @@ if not check_password():
     st.stop()
 
 # endregion <--------- Streamlit App Configuration --------->
+st.write("<b>IMPORTANT NOTICE:</b> This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
 st.title("Competency Tagger App")
 st.write("<b>IMPORTANT NOTICE:</b> This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
 
