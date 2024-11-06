4217b94ecde7a1a7e628687ec66685551d838fe1
tipsyelifant <deirepromissio@gmail.com>
2024-11-06 12:06:03 +0800

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -1,2 +1,8 @@
+import streamlit as st
+import pandas as pd
+import openai
+from pinecone import Pinecone
+from utility import check_password
+
 st.title("Project Scope:")
 st.write("The Competency Tagger project aims to develop an AI-powered platform to automate the process of tagging learning programs with relevant competencies from various Competency Frameworks. This will streamline program management for Institute Development Officers (IDOs), freeing up their time for higher-value tasks like program development and evaluation.")
