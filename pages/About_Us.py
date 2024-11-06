acc0aae30ebaa1586499c2a5a83bed74fa931a06
tipsyelifant <deirepromissio@gmail.com>
2024-11-06 12:15:41 +0800

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -13,3 +13,11 @@ st.subheader("Objectives:")
 st.write("**Automate Competency Tagging:** Develop an LLM-based system that efficiently tags learning programs with relevant competencies.")
 st.write("**Improve Efficiency:** Significantly reduce the time IDOs spend on manual competency tagging.")
 st.write("**Enhance Program Management:** Enable IDOs to focus on developing, improving, and evaluating learning programs.")
+
+st.subheader("Data Source:")
+st.write("**Competency Frameworks:** The platform will utilise existing competency frameworks.")
+
+st.subheader("Features:")
+st.write("**Intelligent Tagging:** The LLM will analyze learning program descriptions, course activities and instructional methods and suggest relevant competency tags.")
+st.write("**User-Friendly Interface:** IDOs will interact with an intuitive interface to review and refine suggested tag.")
+st.write("**Proof-of-Concept:** Explore potential concept for tagging of programmes to various competency frameworks.")
